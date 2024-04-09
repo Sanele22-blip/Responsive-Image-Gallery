cd463b9bbd18a92a1a98063e6200f478442e5737
Sanele <sanelexegwana@gmail.com>
2024-04-09 17:10:49 +0200

    Updated html, css and js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
-const modal = document.getElementById('modal');
-    const modalImg = document.getElementById('modal-img');
-    const images = document.querySelectorAll('.images img');
-    const prevBtn = document.getElementById('prevBtn');
-    const nextBtn = document.getElementById('nextBtn');
-    let currentIndex = 0;
+function filterImages(category) {
+    const allImages = document.querySelectorAll('.category');
 
-    images.forEach((img, index) => {
-        img.addEventListener('click', () => {
-            currentIndex = index;
-            modal.style.display = 'block';
-            modalImg.src = img.src;
-        });
-    });
+    allImages.forEach(imageGroup => {
+        const categoryTitle = imageGroup.querySelector('h2').textContent;
 
-    modal.addEventListener('click', (event) => {
-        if (event.target === modal || event.target.classList.contains('close')) {
-            modal.style.display = 'none';
+        if (category === 'All' || category === categoryTitle) {
+            imageGroup.style.display = 'block';
+        } else {
+            imageGroup.style.display = 'none';
         }
     });
-
-    function showImage(index) {
-        modalImg.src = images[index].src;
-    }
-
-    prevBtn.addEventListener('click', () => {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        showImage(currentIndex);
-    });
-
-    nextBtn.addEventListener('click', () => {
-        currentIndex = (currentIndex + 1) % images.length;
-        showImage(currentIndex);
-    });
+}
